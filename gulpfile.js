f9d2226d9cf30dd6a123c0f4e3f4f9b8ff97b240
FollowWinter <lyxxxx@yeah.net>
2016-12-06 17:50:26 +0800

    1. 修改整体颜色 2. 添加default 3. 添加gulp用来打包项目

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var del = require('del');
+var browsersync = require('browser-sync').create();
+var reload = browsersync.reload;
+var sass = require('gulp-sass');
+var min_css = require('gulp-minify-css');
+var rename = require('gulp-rename');
+/**
+ * 监听文件变化并刷新浏览器
+ */
+gulp.task('watch', function () {
+    browsersync.init({
+        server: {
+            baseDir: "."
+        }
+    });
+    gulp.watch("src/**/*.scss", ['sassfile']).on('change', reload);
+    gulp.watch("src/excamp/*.html").on('change', reload);
+});
+/**
+ * 将sass转化为css
+ */
+gulp.task('sassfile', function () {
+    return gulp.src('./src/emui.scss')
+        .pipe(sass())
+        .pipe(min_css())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist'));
+});
+
+/**
+ * 打包压缩的css
+ */
+gulp.task('build', function () {
+    return gulp.src('./src/emui.scss')
+        .pipe(sass())
+        .pipe(min_css())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist'));
+})
