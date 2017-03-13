d2caa333c53ec1cda6e82d21f0692610d5900820
FollowWinter <lyxxxx@yeah.net>
2017-03-13 15:48:02 +0800

    - 添加`postcss`工具,兼容旧版本浏览器

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-var gulp = require('gulp');
-var del = require('del');
-var browsersync = require('browser-sync').create();
-var reload = browsersync.reload;
-var sass = require('gulp-sass');
-var min_css = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var bump = require('gulp-bump')
+var gulp         = require('gulp');
+var del          = require('del');
+var browsersync  = require('browser-sync').create();
+var reload       = browsersync.reload;
+var sass         = require('gulp-sass');
+var min_css      = require('gulp-minify-css');
+var rename       = require('gulp-rename');
+var bump         = require('gulp-bump')
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnext      = require('cssnext');
+var precss       = require('precss');
 /**
  * 监听文件变化并刷新浏览器
  */
@@ -38,6 +42,10 @@ gulp.task('bump', function () {
 gulp.task('build', function () {
     return gulp.src('./index.scss')
         .pipe(sass())
+        .pipe(postcss([
+            autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'),
+            cssnext,
+            precss]))
         .pipe(min_css())
         .pipe(rename('emui.min.css'))
         .pipe(gulp.dest('./dist'));
